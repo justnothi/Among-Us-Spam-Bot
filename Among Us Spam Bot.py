2fbb21afb55f42ffea3b95eba83f70e72821fe1d
justnothi <80005071+justnothi@users.noreply.github.com>
2021-03-08 14:42:35 -0500

    Update Among Us Spam Bot.py

--- Among Us Spam Bot.py
+++ Among Us Spam Bot.py
@@ -6,7 +6,7 @@ sleep(2)
 for i in range(20):
 	pyautogui.click(712,600)
 	sleep(0.5)
-	pyautogui.typewrite("ALL ARE IMPOSTOR")
+	pyautogui.typewrite("https://www.youtube.com/watch?v=OkEPEjl1Kkk")
 	pyautogui.press("enter")
 	sleep(0.5)
 	pyautogui.press("enter")
